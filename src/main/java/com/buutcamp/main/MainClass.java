5ac4b563359345f46750fbb45a686fb8ca9bfcec
heidihuht <veera2904>
2018-10-11 12:02:16 +0300

    initial

---
+++ src/main/java/com/buutcamp/main/MainClass.java
@@ -0,0 +1,8 @@
+package com.buutcamp.main;
+
+public class MainClass {
+
+    public static void main(String[] args){
+        RunApp runApp = new RunApp();
+    }
+}
---
+++ src/main/java/com/buutcamp/main/RunApp.java
@@ -0,0 +1,27 @@
+package com.buutcamp.main;
+
+import com.buutcamp.inputhandle.HandleInput;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLOutput;
+
+public class RunApp {
+
+    public RunApp() {
+
+        HandleInput handleInput = new HandleInput();
+        while (true) {
+
+            String userInput = handleInput.getUserInput();
+
+            System.out.println(userInput);
+            if (userInput.equals("exit")) {
+                return;
+            }
+
+
+        }
+    }
+}
